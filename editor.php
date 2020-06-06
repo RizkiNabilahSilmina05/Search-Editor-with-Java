2bb63098dc09ea74f1c333e511bc4cf16db1946b
RizkiNabilahSilmina05 <63065064+RizkiNabilahSilmina05@users.noreply.github.com>
2020-06-06 19:48:03 +0700

    Update editor.php

--- editor.php
+++ editor.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Data Editor</title>
-    </head>
-    <body>
-        <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #061C30;">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #008B8B;">
         <div class="container">
-            <a class="navbar-brand font-weight-bold" href="index.php"><i class="fas fa-edit"></i> DATA EDITOR</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              	<span class="navbar-toggler-icon"></span>
-            </button>
+            <a class="navbar-brand font-weight-bold"><i class="fas fa-edit"></i> DATA EDITOR</a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+					<a class="nav-link" href="index.php">Home</a>
+					</li>
 					<li class="nav-item active">
-					<a class="nav-link" href="editor.php">Editor</a>
+					<a class="nav-link" href="editor.php">Data</a>
 					</li>
 					<li class="nav-item active">
 					<a class="nav-link" href="about.php">About Me</a>
@@ -26,40 +25,38 @@
             </div>
         </div>
 	</nav>
-        <!-- End Navbar -->
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col">
-                    <div class="input-group mb-3 mt-5 w-50 mx-auto">
-                        <input aria-describedby="button-addon2" aria-label="Recipient's username" class="form-control input" placeholder="Editor Name" type="text">
-                            <div class="input-group-append">
-                                <a class="btn btn-primary search text-white" id="button-addon2" type="button">
-                                    <span class="fas fa-search fa-sm">Search</span>
-                                </a>
-                            </div>
-                        </input>
-                    </div>
-                    <hr class="my-4">
 
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="input-group mb-3 mt-5 w-50 mx-auto">
+                    <input aria-describedby="button-addon2" aria-label="Recipient's username" class="form-control input" placeholder="Editor Name" type="text">
+                        <div class="input-group-append">
+                            <a class="btn btn-primary search text-white" id="button-addon2" style ="background-color:mediumaquamarine" type="button">
+                                <span class="fas fa-search fa-sm">Search</span>
+                            </a>
+                        </div>
+                    </input>
                 </div>
+                <hr class="my-4">
             </div>
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="result d-flex justify-content-center mt-4"></div>
-                </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="result d-flex justify-content-center mt-4"></div>
             </div>
         </div>
+    </div>
 
-        <script src="assets\js\bootstrap.min.js"></script>
-        <script src="assets\js\jquery.min.js"></script>
-
-    </body>
+    <script src="assets\js\bootstrap.min.js"></script>
+    <script src="assets\js\jquery.min.js"></script>
+</body>
 </html>
+
 <script>
 	loadData();
-
 	function loadData() {
 		$.get('core/get.php?q=show', function(data){
 			$(".result").html(data);
