9688079ea679ac6589203da2c18e72ee9cf14e0a
RizkiNabilahSilmina05 <63065064+RizkiNabilahSilmina05@users.noreply.github.com>
2020-06-06 19:48:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,16 +7,16 @@
     <title>Data Editor</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #061C30;">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #008B8B;">
         <div class="container">
-            <a class="navbar-brand font-weight-bold" href="index.php"><i class="fas fa-edit"></i> DATA EDITOR</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              	<span class="navbar-toggler-icon"></span>
-            </button>
+            <a class="navbar-brand font-weight-bold"><i class="fas fa-edit"></i> DATA EDITOR</a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+					<a class="nav-link" href="index.php">Home</a>
+					</li>
 					<li class="nav-item active">
-					<a class="nav-link" href="editor.php">Editor</a>
+					<a class="nav-link" href="editor.php">Data</a>
 					</li>
 					<li class="nav-item active">
 					<a class="nav-link" href="about.php">About Me</a>
@@ -26,28 +26,14 @@
         </div>
 	</nav>
 	<!-- <div class="container"> -->
-		<div class="text-center mt-3">
-            <h1 class="display-4">Welcome!</h1>
-            <br>
-            <hr>
-			<h5>This website displays editor information on the site detik.com article along with the edited article <br>The data obtained comes from the hash data structure that is integrated with the web</h5>
-            <br>
-            <a class="btn btn-lg" href="https://github.com/mr1dha/java-hash-structure-implemented-to-web" style ="background-color:rosybrown"><i class="fab fa-github" style ="font-family: 'marck script',cursive;"></i> Kunjungi Github</a>
-		</div>
-		<!-- <div class="row justify-content-end">
-			<div class="col-lg-4">
-				<div class="input-group my-3">
-					<input aria-describedby="button-addon2" aria-label="Recipient's username" class="form-control input" placeholder="Editor Name..." type="text">
-					<div class="input-group-append">
-						<a class="btn btn-primary search text-white" id="button-addon2" type="button">
-							<span class="fas fa-search fa-sm">Search</span>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="result text-center"></div> -->
-	<!-- </div> -->
+	<div class="text-center mt-3">
+        <h1 class="display-4">Welcome!</h1>
+        <br>
+        <hr>
+	    <h5>This website displays editor information on the site detik.com article along with the edited article <br>The data obtained comes from the hash data structure that is integrated with the web</h5>
+        <br>
+        <a class="btn btn-lg" href="https://github.com/RizkiNabilahSilmina05/Search-Editor-with-Java" style ="background-color:mediumaquamarine"><i class="fab fa-github" style ="font-family: 'marck script',cursive;"></i> Kunjungi Github</a>
+	</div>
 
 	<script src="assets\js\jquery.min.js"></script>
 	<script src="assets\js\bootstrap.min.js"></script>
